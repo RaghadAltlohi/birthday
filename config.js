ea5ebb0a2b67af3b9e4c3827324b8ab07d308125
RaghadAltlohi <108937154+RaghadAltlohi@users.noreply.github.com>
2023-06-23 01:18:55 +0300

    RRRRconfig.js

--- config.js
+++ config.js
@@ -30,13 +30,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        turn_on: "READ?",
+        play: "HERE WE GO ",
+        bannar_coming: "NEXT",
+        balloons_flying: "HAPPY BIRTHDAY SALEH ",
+        cake_fadein: "BRING THE CAKE ",
+        light_candle: "LIGHT THE CANDEL",
+        wish_message: "MAKE A WISH",
+        story: "I WISH YOU A HAPPY YEAR ",
     }
 };
